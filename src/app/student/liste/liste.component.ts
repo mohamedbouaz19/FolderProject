56c33a536a30a5de322c12bdcc77544f9b550878
mohamed <meedbouazza09@gmail.com>
2024-10-11 12:53:21 +0100

    first commit

--- src/app/student/liste/liste.component.ts
+++ src/app/student/liste/liste.component.ts
@@ -5,15 +5,14 @@ import { CommonModule } from '@angular/common';
   selector: 'app-liste',
   standalone: true,
   imports: [CommonModule],
+
   templateUrl: './liste.component.html',
   styleUrl: './liste.component.css'
 })
 export class ListeComponent {
 students =[
-  {"id":1,"nom":"HARIII","note":18},
+  {"id":1 ,"nom":"HARIII","note":18},
   {"id":2,"nom":"Annass","note":14},
   {"id":3,"nom":"HHHmed","note":20},
-
-
 ]
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "./menu/menu.component";
 import { ListeComponent } from "./liste/liste.component";
+import { FormComponent } from "./form/form.component";
 
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [MenuComponent, ListeComponent],
+  imports: [MenuComponent, ListeComponent, FormComponent],
   templateUrl: './student.component.html',
   styleUrl: './student.component.css'
 })
